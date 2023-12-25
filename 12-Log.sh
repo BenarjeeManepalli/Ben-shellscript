a44a0893bcff03de46b1468bd789697663d9c9e7
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 00:19:19 +0530

    new files in to repo

--- 12-Log.sh
+++ 12-Log.sh
@@ -10,27 +10,27 @@ N="\e[0m"   # N is Normal Code.
 
 echo "The Script stated at ${TIMESTAMP}" &>> $LOGFILE
 
-VALIDATE(){
+VALIDATE (){
     if [ $1 -ne 0 ]
     then
-      echo -e "ERROR :: $2 is .... $R FAILED $N"
+      echo -e "ERROR:: $2 is .... $R FAILED $N"
       exit 1
-    else
-       echo -e "$2 is ..... $G SUCCESS $N"  
+       else
+          echo -e "$2 is ..... $G SUCCESS $N"  
     fi  # end of the condition.  
 }      
 
 if [ $ID -ne 0 ]
 then
-    echo "$R ERROR :: You are not root user please proceed with root $N"
+    echo -e "$R ERROR :: You are not root user please proceed with root $N"
     exit 1 # 1 is the failuer excuted command
 else
-    echo "$G You are the Root user proceeding the installtion of the package"
+    echo -e "$G You are the Root user proceeding the installtion of the package"
 fi  #fi means end of the condition.
 
 yum install mysql -y &>> $LOGFILE
 
-VALIDATE $? "Installing the mysql" &>> LOGFILE # Installing the sql package
+VALIDATE $? "Installing the mysql" &>> $LOGFILE # Installing the sql package
 
 yum install git -y &>> $LOGFILE #installing the git package
 
