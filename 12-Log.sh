7561788a7a5da428e0e7fad3db45eb281e4aecd4
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-25 23:46:28 +0530

    new files in to repo

--- 12-Log.sh
+++ 12-Log.sh
@@ -2,9 +2,9 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F==%X) # %F means full date and %X means full time == diffrenciate between date and time
 LOGFILE=(/tmp/$0-$TIMESTAMP.log) # $0 denotes of the file name displaying, log file is storing the tmp location
-R="\e[31m"  # R is Red Code.
-G="\e[32m"  # G is Green Code.
-Y="\e[33m"  # Y is Yello Code.
+R="\\e[1;31m"  # R is Red Code.
+G="\e[1;32m"  # G is Green Code.
+Y="\e[1;33m"  # Y is Yello Code.
 N="\e[0m"   # N is Normal Code.
 
 echo "The Script stated at ${TIMESTAMP}" &>>$LOGFILE
