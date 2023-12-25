2feae5f2fd8a7c95bf706520a77299422f97c27b
BenarjeeManepalli <benarjee.manepalli@gmail.com>
2023-12-26 00:32:31 +0530

    new files in to repo

--- 12-Log.sh
+++ 12-Log.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
-TIMESTAMP=$(date +%F==%X) # %F means full date and %X means full time == diffrenciate between date and time
+TIMESTAMP=$(date +%F-%X) # %F means full date and %X means full time == diffrenciate between date and time
 LOGFILE="/tmp/$0-$TIMESTAMP.log" # $0 denotes of the file name displaying, log file is storing the tmp location
 R="\e[31m"  # R is Red Code.
 G="\e[32m"  # G is Green Code.
 Y="\e[33m"  # Y is Yello Code.
 N="\e[0m"   # N is Normal Code.
 
-echo "The Script stated at ${TIMESTAMP}" &>> $LOGFILE
+echo "$G The Script stated at ${TIMESTAMP}" &>> $LOGFILE
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -28,10 +28,10 @@ else
     echo -e "$G You are the Root user proceeding the installtion of the package"
 fi  #fi means end of the condition.
 
-yum install mysql -y &>> $LOGFILE
+dnf install mysql -y &>> $LOGFILE
 
 VALIDATE $? "Installing the mysql" &>> $LOGFILE # Installing the sql package
 
-yum install git -y &>> $LOGFILE #installing the git package
+dnf install git -y &>> $LOGFILE #installing the git package
 
 VALIDATE $? "installing the git" &>> $LOGFILE  #storing the output in to log.
